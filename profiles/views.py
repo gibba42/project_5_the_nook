ef5375146a1d53f5a73a379c3e06f474bc063505
gibba42 <alastairgibb.kent@gmail.com>
2026-08-09 01:45:51 +0100

    Fixed a bug where users could see all historical orders, not just their own.

--- profiles/views.py
+++ profiles/views.py
@@ -33,8 +33,13 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
 def order_history(request, order_number):
-    order = get_object_or_404(Order, order_number=order_number)
+    order = get_object_or_404(
+        Order,
+        order_number=order_number,
+        user_profile=request.user.userprofile
+    )
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
