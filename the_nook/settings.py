740f0cb224b75d4f6250af07e9ee725b57662179
gibba42 <alastairgibb.kent@gmail.com>
2026-08-10 21:47:03 +0100

    Added a custom 404 page.

--- the_nook/settings.py
+++ the_nook/settings.py
@@ -45,7 +45,7 @@ ALLOWED_HOSTS = [
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
