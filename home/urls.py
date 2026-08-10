e5ca73e748b95243f548c1a7ffc216db993ac31b
gibba42 <alastairgibb.kent@gmail.com>
2026-08-10 21:39:28 +0100

    Updated the robots.txt file for SEO optimisation.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,19 @@
-from django.contrib import admin
+"""URL configuration for the home application."""
+
 from django.urls import path
+
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='home'),
+    path(
+        "",
+        views.index,
+        name="home",
+    ),
+    path(
+        "robots.txt",
+        views.robots_txt,
+        name="robots_txt",
+    ),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+"""Views for The Nook home application."""
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+
 
 def index(request):
-    """ A view to return the index page """
-    
-    return render(request, 'home/index.html')
+    """Return the home page."""
+
+    return render(request, "home/index.html")
+
+
+def robots_txt(request):
+    """Return crawling instructions for search engine robots."""
+
+    sitemap_url = request.build_absolute_uri(
+        reverse("django.contrib.sitemaps.views.sitemap")
+    )
+
+    lines = [
+        "User-agent: *",
+        "Allow: /",
+        "Disallow: /admin/",
+        "Disallow: /accounts/",
+        "Disallow: /bag/",
+        "Disallow: /checkout/",
+        "Disallow: /profile/",
+        "Disallow: /authors/dashboard/",
+        "Disallow: /authors/profile/",
+        "Disallow: /books/manage/",
+        "Disallow: /newsletter/",
+        f"Sitemap: {sitemap_url}",
+    ]
+
+    return HttpResponse(
+        "\n".join(lines),
+        content_type="text/plain",
+    )
